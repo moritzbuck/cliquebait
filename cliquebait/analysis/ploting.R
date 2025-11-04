31d10664ff610973e8b3a845f730084b9c4b5e07
moritzbuck <mrtz.buck@gmail.com>
2025-11-04 21:26:08 +0100

    merged conflict on my vs codes

--- cliquebait/analysis/ploting.R
+++ cliquebait/analysis/ploting.R
@@ -1,11 +1,22 @@
 library(data.table)
 library(ggplot2)
+
 full = fread("/home/moritz/data/0079_pelaginet/cliquebait_nf/cliquebait_full.csv")
 summ = fread("/home/moritz/data/0079_pelaginet/cliquebait_nf/cliquebait_summary.csv")
 
-kept = summ[stab_clique_ani < 95 & nb_genomes > 50]$V1
+kept = summ[stab_clique_ani < 95 & nb_genomes > 30]$V1
 ggplot(full[cluster_id %in% kept], aes(x = ani, group=cluster_id, coll=family))+geom_density()+facet_wrap(~phylum)
 
+ggplot(full[cluster_id %in% kept], 
+       aes(x = ani, group = cluster_id, coll = family)) +
+  geom_density() +
+  geom_vline(xintercept = 95, col = "red")
+
+ggplot(summ, aes(x=mean_ani, y=after_stat(ndensity), col = stab_clique_ani < 95)) +
+  geom_freqpoly()
+
+ggplot(summ, aes(x=mean_ani, col = stab_clique_ani < 95)) +
+  geom_freqpoly()
 
 qc = function(tax){
 gmms = lapply(1:5, function(x) Mclust(full[cluster_id == "g__Campylobacter_B_01"]$ani, G=x))
@@ -18,6 +29,8 @@ mean_diffs = sapply(gmms, function(x) max(x$parameters$mean)-min(x$parameters$me
 }
 
 
- ggplot(full[cluster_id %in% kept], aes(x = ani, y=after_stat(ndensity), group=cluster_id, fill = phylum))+
-    geom_histogram(bins =100)+xlim(90,100)+geom_vline(xintercept = 95, col = "red")+
-    facet_wrap(~cluster_id, scale = "free")
+ggplot(full[cluster_id %in% kept],
+       aes(x = ani, y=after_stat(ndensity), group=cluster_id, fill = phylum))+
+  geom_histogram(bins =100)+xlim(90,100)+
+  geom_vline(xintercept = 95, col = "red")+
+      facet_wrap(~cluster_id, scale = "free")
